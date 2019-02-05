e9c403923ccfa0c702ce00f219dc657f599e93de
cierravereen24 <cierravereen24@gmail.com>
2019-02-05 16:27:44 -0600

    unfinished

---
+++ Review.js
@@ -0,0 +1,19 @@
+
+
+
+btn.onclick = function()
+{
+  var key= prompt("Enter 'y' to keep going or 'n' to quit.");
+
+  while
+};
+
+
+
+function myFunction()
+{
+ myFunction = document.getElementsByTagName("button");
+}
+
+while (True)
+    var key= prompt("Enter 'y' to keep going or 'n' to quit.");
